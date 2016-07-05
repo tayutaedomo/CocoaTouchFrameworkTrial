6dd474e9dbafb3b743f789ad23eaa5fc25209e97
tayutaedomo <tayutaedomo@gmail.com>
2016-07-06 02:04:05 +0900

    Add MyFramework

---
+++ MyFramework/MyFramework.swift
@@ -0,0 +1,15 @@
+//
+//  MyFramework.swift
+//  CocoaTouchFrameworkTrial
+//
+//  Created by tayutaedomo on 2016/07/06.
+//  Copyright © 2016年 tayutaedomo.net. All rights reserved.
+//
+
+import Foundation
+
+public class MyFramework: NSObject {
+    public class func start(){
+        print("MyFramework starting...")
+    }
+}
